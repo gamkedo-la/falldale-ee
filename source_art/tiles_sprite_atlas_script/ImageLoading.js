76af0e5e6971afc6c396e93d6f135a5e2db329eb
Gamkedo <chris@gamkedo.com>
2020-05-03 18:21:34 -0700

    adding code to source_art subdirectory I wrote for packing the tiles, in case of use for combining some others, or to regenerate/modify the tiles sheet

---
+++ source_art/tiles_sprite_atlas_script/ImageLoading.js
@@ -0,0 +1,108 @@
+var baseName = ["1 grave","2 graves",//"20sided",
+                "OpenDoorway","alchemist","alchemist1",//"arrow",
+                //"arrow2",
+                "arrowOld","backwall","backwallcabinetlowerhalf","backwallcabinettophalf","backwallemptycabinet","backwallrightside","bar2",//"basicSword",
+                "bedleftside","bedleftsidets",//"bite",
+                "blacksmith","blacksmith1",
+                //"bonfire_l_PH","bonfire_r_PH",
+                "bonfire_s_PH",
+                //"bonfire_unlit_l","bonfire_unlit_r",
+                "bonfire_unlit_s",
+                //"bow",
+                "box","brickWindow","brickwall_tl","bridge-lower","bridge-upper","brokenBox","bush","bush2","bush_tall","campring_lg_l","campring_lg_r","cart","cart_broken","castle_battlement_b1","castle_battlement_b2","castle_battlement_btl","castle_battlement_btr","castle_battlement_l1","castle_battlement_l2","castle_battlement_l3","castle_battlement_ltb","castle_battlement_ltt1","castle_battlement_ltt2","castle_battlement_mc1","castle_battlement_mc10","castle_battlement_mc11","castle_battlement_mc2","castle_battlement_mc3","castle_battlement_mc4","castle_battlement_mc5","castle_battlement_mc6","castle_battlement_mc7","castle_battlement_mc8","castle_battlement_mc9","castle_battlement_r1","castle_battlement_r2","castle_battlement_r3","castle_battlement_rtb","castle_battlement_rtt","castle_battlement_t","castle_battlement_ttl","castle_battlement_ttr1","castle_battlement_ttr2","castle_bg1","castle_bg10","castle_bg11","castle_bg12","castle_bg13","castle_bg14","castle_bg15","castle_bg16","castle_bg17","castle_bg18","castle_bg19","castle_bg2","castle_bg20","castle_bg21","castle_bg22","castle_bg23","castle_bg3","castle_bg4","castle_bg5","castle_bg6","castle_bg7","castle_bg8","castle_bg9","castle_door_b1","castle_door_b2","castle_door_lr1","castle_door_lr10","castle_door_lr11","castle_door_lr12","castle_door_lr13","castle_door_lr14","castle_door_lr15","castle_door_lr16","castle_door_lr2","castle_door_lr3","castle_door_lr4","castle_door_lr5","castle_door_lr6","castle_door_lr7","castle_door_lr8","castle_door_lr9","castle_door_t1","castle_door_t2","castle_gate1_bl","castle_gate1_br","castle_gate1_tl","castle_gate1_tr","castle_gate2_bl","castle_gate2_br","castle_gate2_tl","castle_gate2_tr","castle_gate3_bl","castle_gate3_br","castle_gate3_tl","castle_gate3_tr","castle_gate4_bl","castle_gate4_br","castle_gate4_tl","castle_gate4_tr","castle_gate5_tl","castle_gate5_tr","castle_pillar_bl","castle_pillar_br","castle_pillar_ml","castle_pillar_mr","castle_pillar_tl","castle_pillar_tr","castle_wall_b1","castle_wall_b2","castle_wall_b3","castle_wall_btl1","castle_wall_btl2","castle_wall_btl3","castle_wall_btl4","castle_wall_btl5","castle_wall_btr1","castle_wall_btr2","castle_wall_btr3","castle_wall_btr4","castle_wall_l1","castle_wall_l2","castle_wall_l3","castle_wall_l4","castle_wall_l5","castle_wall_lr1","castle_wall_lr2","castle_wall_lr3","castle_wall_lr4","castle_wall_lr5","castle_wall_lr6","castle_wall_lr7","castle_wall_lr8","castle_wall_ltb1","castle_wall_ltb2","castle_wall_ltb3","castle_wall_ltb4","castle_wall_ltt1","castle_wall_ltt2","castle_wall_mc1","castle_wall_r1","castle_wall_r2","castle_wall_r3","castle_wall_r4","castle_wall_r5","castle_wall_rtb1","castle_wall_rtb2","castle_wall_rtb3","castle_wall_rtb4","castle_wall_rtb5","castle_wall_rtt1","castle_wall_rtt2","castle_wall_rtt3","castle_wall_rtt4","castle_wall_rtt5","castle_wall_tc1","castle_wall_tc2","castle_wall_tc3_b","castle_wall_tc3_t","castle_wall_tc4_b","castle_wall_tc4_t","castle_wall_tc5","castle_wall_ttl1","castle_wall_ttl2","castle_wall_ttl3","castle_wall_ttl4","castle_wall_ttl5","castle_wall_ttr1","castle_wall_ttr2","castle_window1","castle_window2","castleb_gate1_tl","castleb_gate1_tr","castleb_gate2_tl","castleb_gate2_tr","castleb_gate3_tl","castleb_gate3_tr","castleb_gate4_tl","castleb_gate4_tr","castleb_gate5_tl","castleb_gate5_tr","castleb_wall_l1","castleb_wall_l2","castleb_wall_l3","castleb_wall_l4","castleb_wall_l5","castleb_wall_mc1","castleb_wall_r1","castleb_wall_r2","castleb_wall_r3","castleb_wall_r4","castleb_wall_r5","castleb_window2","cave_lg_bl","cave_lg_br","cave_lg_tl","cave_lg_tr","cave_sm","cave_smd","cliff","cliffBottomLeft","cliffFullShadow","cliffLeft","cliffRight","cliffRightCorner","cliffShadow","cliffTop","cliffTop1","cliffTopLeft","cliffTopRightCorner","cliff_b","cliff_c_btl","cliff_c_btr","cliff_c_ltb","cliff_c_ltt","cliff_c_rtb","cliff_c_rtt","cliff_c_ttl","cliff_c_ttr","cliff_l","cliff_r","cliff_stairs_l","cliff_stairs_u","cliff_t","cloth",
+                //"club",
+                "collider_l","dcliff_bb","dcliff_bt","dcliff_c_btl","dcliff_c_rtb","dcliff_j_double_bl","dcliff_j_double_br","dcliff_j_double_tl","dcliff_j_double_tr","dcliff_j_single_l","dcliff_j_single_r","dcliff_lc_btr","dcliff_lc_ltb","dcliff_uc_btr","dcliff_uc_ltb","dresserbottom","dressertop",
+                //"druidcircle",
+                "druidcircle1","druidcircle2","druidcircle3","druidcircle4","druidcircle5","druidcircle6","druidcircle7","druidcircle8","druidcircle9","druidhouse_fl","druidhouse_fm","druidhouse_fr","druidhouse_ml","druidhouse_mr","druidhouse_tl","druidhouse_tm","druidhouse_tr","fence","fencebottomleftpost","fencebottomrightpost","fenceleftpost","fenceleftside","fencerightCorner","fencerightpost","fenceside","fencetopleftpost","fencetoprightpost",
+                //"fireball",
+                "fountain","frontdoor","garden_1","garden_2","garden_3","garden_4","garden_5","garden_6","garden_7","garden_8","garden_9",
+                //"gold",
+                "grass","grass_b","grass_btl","grass_btr","grass_l","grass_ltb","grass_ltt","grass_r","grass_rtb","grass_rtt","grass_t","grass_ttl","grass_ttr","grave_3","healingPotion",
+                //"heart",
+                "houseWall","houseWindow","join_bar_wwall_l","join_bar_wwall_r","large grave","lflagstone1","lflagstone2",
+                //"magicSword",
+                "mausoleumB_1","mausoleumB_2","mausoleumB_3","mausoleumB_4","mausoleum_1","mausoleum_2","mausoleum_3","mausoleum_4","oblisk grave","orc_hut_bl","orc_hut_br","orc_hut_tl","orc_hut_tr","pub",
+                //"rock",
+                "roofFrontRight","roofbackleft","roofbackright","roofbackside","roofbottomright","roofcenter","rooffront","rooffrontleft","roofleftside","stool","table","throwingRocks","treasure",
+                //"tree",
+                "tree1","tree2","tree2bottomhalf","tree2tophalf","tree3","tree3bottomhalf","tree3tophalf","tree4","tree4bottomhalf","tree4tophalf","tree5FallenBottom","tree5FallenTopHalf","treeA1","treeA2","treeA3","treeA4",
+                //"tree_pine_blue_l","tree_pine_blue_r","tree_pine_l","tree_pine_r","tree_pine_yellow_l","tree_pine_yellow_r",
+                "twigs","water","waterFallPlaceholder","waterPlaceholder","weaponsrack","weaponsracklh","woodwall_bc","woodwall_bl","woodwall_br","woodwall_ml","woodwall_mr","woodwall_tc","woodwall_tl","woodwall_tr","woodwall_ul","woodwall_wdw_tc","worldRoad"];
+
+var dimW = 50;
+var dimH = 50;
+
+var cols = 21;
+var rows = 20;
+
+var animPics = [];
+
+var picsToLoad = 0;
+
+var canvas, canvasContext;
+
+window.onload = function() {
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+  
+  loadImages();
+}
+
+function loadingDoneSoStartGame() {
+
+  canvas.width = dimW*cols;
+  canvas.height = dimH*rows;
+
+  var tileCount=0;
+  for(var i=0;i<animPics.length;i++) {
+    if(Math.abs(dimW-animPics[i].width)>3 || Math.abs(dimH-animPics[i].height)>3) { // including ones that were off by 1 or 2 pixels
+      console.log(i+" non standard: "+animPics[i].src + " W:" + animPics[i].width + " H:" + animPics[i].height);
+      continue;
+    }
+    canvasContext.drawImage(animPics[i],
+      0,0,dimW,dimH,
+      dimW*(tileCount%cols),dimH*Math.floor(tileCount/cols),dimW,dimH);
+    tileCount++;
+  }
+}
+
+function countLoadedImageAndLaunchIfReady() {
+  picsToLoad--;
+  console.log(picsToLoad);
+  if(picsToLoad == 0) { // last image loaded?
+    loadingDoneSoStartGame();
+  }
+}
+
+function beginLoadingImage(imgVar, fileName) {
+  imgVar.onload=countLoadedImageAndLaunchIfReady;
+  imgVar.src=fileName;
+}
+
+function loadImageForAnimCode(animCode, fileName) {
+  animPics[animCode] = document.createElement("img");
+  beginLoadingImage(animPics[animCode],fileName);
+}
+
+function frameIndex(facing, anim) {
+  return facing + anim*animFacings;
+}
+
+function loadImages() {
+
+  var imageList = [
+    ];
+
+  for(var i=0;i<baseName.length;i++) {
+      var filename = "tiles/"+baseName[i]+".png";
+      // console.log(filename);
+      imageList.push({animType:i, theFile:filename});
+  }
+
+  picsToLoad = imageList.length;
+
+  for(var i=0;i<imageList.length;i++) {
+    loadImageForAnimCode(imageList[i].animType, imageList[i].theFile);
+  } // end of for imageList
+
+} // end of function loadImages
